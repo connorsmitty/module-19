ba2c76b2f0fcc3db7fdc8f79d03e259dd39f8a06
connorsmitty <93891866+connorsmitty@users.noreply.github.com>
2024-04-15 18:44:22 -0400

    Lots of changes

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,38 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior
+  event.preventDefault();
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // Save the event object so it can be used later
+  window.deferredPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Show the install button
+  butInstall.style.display = 'block';
+});
+
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  // Hide the install button
+  butInstall.style.display = 'none';
+
+  // Show the install prompt
+  window.deferredPrompt.prompt();
+
+  // Wait for the user to respond to the prompt
+  const { outcome } = await window.deferredPrompt.userChoice;
+
+  // Clear the deferredPrompt variable
+  window.deferredPrompt = null;
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  // Hide the install button
+  butInstall.style.display = 'none';
+
+  // Log the event
+  console.log('The app was installed', event);
+});
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,28 +3,87 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+// Add and configure workbox plugins for a service worker and manifest file.
+const workboxConfig = {
+  swSrc: './src/sw.js',
+  swDest: 'sw.js',
+  include: [/\.js$/, /\.css$/],
+  exclude: [/node_modules/],
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\//,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24,
+        },
+      },
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+  ],
+};
 
-    module: {
-      rules: [
-        
+// Add CSS loaders and babel to webpack.
+const cssLoaders = [
+  'style-loader',
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => [require('autoprefixer')],
+    },
+  },
+];
+
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    presets: ['@babel/preset-env'],
+  },
+};
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'A progressive web app built with webpack',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
       ],
-    },
-  };
+    }),
+    new InjectManifest(workboxConfig),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: babelLoader,
+      },
+    ],
+  },
 };
